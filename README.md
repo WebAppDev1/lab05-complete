13b2f367959bef300808b841f9f054e4c9a043ac
Glitch (rb-lab05-webapp-2020) <none>
2020-02-05 11:16:54 +0000

    🐟🏝 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# lab05-complete
+Lab 5
+=================
+
+- Changed dashboard to show playlist summary
+- Added playlist view to show individual playlist
+- Added delete song button
+- Used lodash to simplify get playlist function
+- Added delete playlist function
---
+++ routes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// import express and initialise router
+const express = require('express');
+const router = express.Router();
+
+// import controllers
+const start = require('./controllers/start.js');
+const dashboard = require('./controllers/dashboard.js');
+const about = require('./controllers/about.js');
+const playlist = require('./controllers/playlist.js');
+
+// connect routes to controllers
+router.get('/', start.index);
+router.get('/dashboard', dashboard.index);
+router.get('/about', about.index);
+router.get('/playlist/:id', playlist.index);
+router.get('/playlist/:id/deleteSong/:songid', playlist.deleteSong);
+router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
+
+// export router module
+module.exports = router;
+
